77f35a5c79d1f278e781bb448f7127fbf25f5520
ValeraZhukevich <paramone.val@gmail.com>
2023-02-12 12:23:52 +0300

    Update amazon test

--- src/test/java/config/SelenoidConfig.java
+++ src/test/java/config/SelenoidConfig.java
@@ -1,11 +1,13 @@
 package config;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
@@ -21,7 +23,6 @@ public class SelenoidConfig implements BeforeEachCallback, BeforeAllCallback {
                 "enableVNC", true,
                 "enableVideo", true
         ));
-
         Configuration.browserCapabilities = capabilities;
         Configuration.remote = "http://34.88.77.186:4444/wd/hub";
     }
--- src/test/java/gui/AmazonTest.java
+++ src/test/java/gui/AmazonTest.java
@@ -1,6 +1,5 @@
 package gui;
 
-import com.codeborne.selenide.Condition;
 import config.SelenoidConfig;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -16,13 +15,21 @@ public class AmazonTest {
     @Tag("gui")
     @Test
     void searchByProductName() {
-        open("https://amazon.com");
-        $("#twotabsearchtextbox").val("Star wars lego").pressEnter();
+        open("https://oz.by");
+        $(".styles_primaryButton__30pEq").click();
+        sleep(6000);
+        $("#catalogSearch").val("Star wars lego").pressEnter();
+        $x("//label[@title='Lego']").click();
+        $("#j-filter__btn").click();
         $x("//span[text()='$200 & Above']").click();
         $(".s-card-container").click();
         $("#productTitle").shouldHave(text("LEGO Star Wars Millennium Falcon 75257"));
         $("#add-to-wishlist-button-group").click();
         $(By.name("signIn")).shouldBe(visible);
         sleep(5000);
+
+//        open("https://oz.by");
+//        sleep(50000);
+//        $("#top-s").val("Star wars lego").pressEnter();
     }
 }
